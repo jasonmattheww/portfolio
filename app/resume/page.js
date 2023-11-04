c6d4e4ff8f69743f14a22f55e0887df847c380ab
Jason <jasontorrealba199@gmail.com>
2023-11-04 12:58:20 -0400

    Added resume page

---
+++ app/resume/page.js
@@ -0,0 +1,36 @@
+'use client'
+
+import { Box, Center, AspectRatio, Link } from '@chakra-ui/react';
+import NavBar from '../../components/NavBar';
+import Transition from '../../components/Transition';
+
+const Resume = () => {
+  const linkStyles = {
+    color: 'black',
+    textDecoration: 'none',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
+
+  return (
+    <Box textAlign="center" position='relative'>
+      <NavBar />
+      <Transition>
+
+        <Center height="85vh" position='relative'>
+          <AspectRatio ratio={4 / 4} maxW='800px' w='100%' h='88%'>
+            <iframe src="resume.pdf#view=fitH" width="100%" height="100%" title="Resume"></iframe>
+          </AspectRatio>
+        </Center>
+
+        <Link href="Resume.pdf" download {...linkStyles} _hover={{ textDecoration: 'underline' }}>
+          Download Resume
+        </Link>
+      </Transition >
+    </Box>
+  );
+};
+
+export default Resume;
+
